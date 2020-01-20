c28da2f614273a1acf0996694c5ed129327d04f9
HNguye005 <HNguye005@COSWL-C045177.cable.comcast.com>
2020-01-20 16:37:58 -0700

    Commit

---
+++ src/main/java/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,51 @@
+package org.totalbeginner.tutorial;
+
+import javax.print.DocFlavor.STRING;
+
+public class Person {
+	//fields
+	private String name;
+	private int maximumBooks;
+	
+	//constructor
+	public Person() {
+		name = "Unknown";
+		maximumBooks = 3;
+	}
+	
+	//methods
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String anyName) {
+		name = anyName;
+	}
+
+	public int getMaximumBooks() {
+		return maximumBooks;
+	}
+
+	public void setMaximumBooks(int maximumBooks) {
+		this.maximumBooks = maximumBooks;
+	}
+
+	///* ---- To test --------------------
+
+	public static void main(String[] args) {
+		//Declare and Instantiate person
+		Person person = new Person();
+		String l_var = "no match";
+		
+		person.setName("dan");
+						
+		if (person.name.equalsIgnoreCase("Dan")) {
+			l_var = "match for Dan";
+		}
+		
+		System.out.println(person.name);
+		System.out.println(l_var);
+	}
+	
+	//-----------------------------------*/
+}
